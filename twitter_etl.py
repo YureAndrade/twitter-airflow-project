e54ea6a798b5e13b3fcc5ee506a74fab1ea79c45
yure-andrade <yureqandrade@gmail.com>
2022-10-12 19:07:13 -0400

    etl and dag updated

--- twitter_etl.py
+++ twitter_etl.py
@@ -3,37 +3,33 @@ import pandas as pd
 import json
 from datetime import datetime
 import s3fs
+import config
 
 def run_twitter_etl():
-
-	access_key = ""
-	access_secret = ""
-	consumer_key = ""
-	consumer_secret = ""
-
 	# Twitter authentication // Autenticando no Twitter
-	auth = tweepy.OAuthHandler(access_key, access_secret)
-	auth.set_access_token(consumer_key, consumer_secret)
-
+	auth = tweepy.OAuthHandler(config.API_KEY, config.API_KEY_SECRET)
+	auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+	
 	# Creating an API object // Criando um objeto API
 	api = tweepy.API(auth)
 	tweets = api.user_timeline(screen_name='@saifedean',
-								count=200,
-								include_rts=True,
-								tweet_mode='extended'
-								)
+		count=2,
+ 		include_rts=False,
+ 		tweet_mode='extended'
+ 	)
 
-	list = []
-	for tweet in tweets:
-		tweet = tweet._json["full_text"]
+twitter_list = []
 
-		refined_tweet = {"user": tweet.user.screen.name,
-						"text": text,
-						"favorite_count": tweet.favorite_count,
-						"created_at": tweet.created_at
-						}
+for tweet in tweets:
+ 	refined_tweet = {"user": tweet.user.screen_name,
+ 	"text": tweet.full_text,
+ 	"favorite_count": tweet.favorite_count,
+ 	"created_at": tweet.created_at,
+ 	"location": tweet.user.location
+ 	}
+ 	#tweet = tweet._json["full_text"]
 
-		list.append(refined_tweet)
+ 	twitter_list.append(refined_tweet)
 
-	df = pd.DataFrame(list)
-	pd.to_csv("refined_tweets.csv")
+df = pd.DataFrame(twitter_list)
+df.to_csv("refined_tweets.csv")
